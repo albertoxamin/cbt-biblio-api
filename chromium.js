6c6e2e8025af5cd228fe765e4604a334f2314c43
Alberto Xamin <alberto@xamin.it>
2019-08-16 11:56:55 +0200

    faster pages

--- chromium.js
+++ chromium.js
@@ -1,14 +1,23 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const chrome = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
 async function myloans(username, password) {
 	const browser = await puppeteer.launch({
 		args: chrome.args,
 		executablePath: await chrome.executablePath,
 		headless: chrome.headless,
-	});
-
-	const page = await browser.newPage();
+	})
+	const page = await browser.newPage()
+	await page.setRequestInterception(true)
+	const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' + 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36'
+	await page.setUserAgent(userAgent)
+	page.on('request', (req) => {
+		if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+			req.abort()
+		} else {
+			req.continue()
+		}
+	})
 	await page.goto('http://www.cbt.biblioteche.provincia.tn.it/oseegenius/workspace?view=myloans')
 	await page.type('input[name=j_username]', username)
 	await page.type('input[name=j_password]', password)
@@ -32,6 +41,28 @@ async function myloans(username, password) {
 		}
 	}
 	return response
+}
+
+async function search(searchTerm) {
+	const browser = await puppeteer.launch({
+		args: chrome.args,
+		executablePath: await chrome.executablePath,
+		headless: chrome.headless,
+	})
+
+	const page = await browser.newPage()
+	await page.goto(`http://www.cbt.biblioteche.provincia.tn.it/oseegenius/search?q=${searchTerm.replace(' ', '+')}&s=25`)
+
+	await page.waitForSelector('.results')
+	const scrapedData = await page.evaluate(() =>
+		Array.from(document.querySelectorAll('.item'))
+			.map(x => ({
+				title: x.querySelector('.title').innerText,
+				edition: x.querySelector('.edition') ? x.querySelector('.edition').innerText : '',
+				publisher: x.querySelector('.publisher').innerText,
+				availability: x.querySelector('.availability').innerText
+			})))
+	return JSON.stringify(scrapedData)
 }
 
 module.exports = { myloans };
